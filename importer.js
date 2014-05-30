7755a3018b74f713bcbf667817ae1bf0613ffc02
Zack <zack@root-two.com>
2014-05-30 14:31:40 -0700

    args

--- importer.js
+++ importer.js
@@ -1,11 +1,19 @@
 var db = require('./db/db.js')
 var CJ = require('cron').CronJob
+var argv = require('yargs')
+
+console.log(argv)
+var dev = argv.dev ? true : false
 
 var spawn = require('child_process').spawn
 
 new CJ('0 */20 * * * *', function(){
   console.log('You will see this message every 20 minute');
-  var py    = spawn('python', ['../tv-import/trendingvalue.py']);
+  var args = ['../tv-import/trendingvalue.py']
+  if(argv.dev) args.push('--dev')
+  if(argv.output) args.push('--output Z'+argv.output)
+
+  var py    = spawn('python', args);
 
   py.stdout.on('data', function (data) {
     console.log('stdout: ' + data);
